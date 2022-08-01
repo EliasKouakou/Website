5c6c5c295278074f54cb9110488acba47254714e
EliasKouakou <98818744+EliasKouakou@users.noreply.github.com>
2022-08-01 09:08:15 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 
 if ("serviceWorker" in navigator){
-    navigator.serviceWorker.register('/service-worker.js').then(registration=>
+    navigator.serviceWorker.register('/Website/service-worker.js').then(registration=>
         {console.log("SW Registration");
         console.log(registration);}).catch(error=>{
             console.log("SW Failed");
